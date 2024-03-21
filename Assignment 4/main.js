b34d8db3f5b948a4bd4b963f6e6984d0e0e12e13
joel5959 <joel.williams@dcmail.ca>
2024-03-21 10:14:15 -0400

    Update main.js
    Finished!

--- Assignment 4/main.js
+++ Assignment 4/main.js
@@ -9,41 +9,51 @@ function randomValueFromArray(array){
 }
 
 /* 2. RAW TEXT STRINGS */
-const storyText="It was 94 fahrenheit outside, so :insertX: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
+const storyText="It was 94 fahrenheit outside, so :insertX: went for a walk. When they got to :insertY:, they stared in horror for a few moments, then :insertZ:. Bob saw the whole thing, but was not surprised — :insertX: weighs 300 pounds, and it was a hot day.";
 
-const insertX= ["Willy the Goblin, Big Daddy, Father Christmas"]
+const insertX = ["Willy the Goblin", 
+                "Big Daddy", 
+                "Father Christmas"
+];
 
-const insertY= ["the soup kitchen, Disneyland, the White House"]
+const insertY= ["the soup kitchen", 
+                "Disneyland", 
+                "the White House"
+];
 
-const insertZ= ["spontaneously combusted, melted into a puddle on the sidewalk, turned into a slug and crawled away"]
+const insertZ= ["spontaneously combusted", 
+                "melted into a puddle on the sidewalk", 
+                "turned into a slug and crawled away"
+];
 
 /* 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITIONS */
 randomize.addEventListener('click', result);
 
 function result() {
-  let newStory = storyText
-  let xItem = randomValueFromArray(insertX)
-  let yItem = randomValueFromArray(insertY)
-  let zItem = randomValueFromArray(insertZ)
+  let newStory = storyText;
 
-  newStory = newStory.replace(":insertX:", xItem)
-  newStory = newStory.replace(":insertY:", yItem)
-  newStory = newStory.replace(":insertZ:", zItem)
+  const xItem = randomValueFromArray(insertX)
+  const yItem = randomValueFromArray(insertY)
+  const zItem = randomValueFromArray(insertZ)
+
+  newStory = newStory.replaceAll(':insertX:', xItem)
+                     .replaceAll(':insertY:', yItem)
+                     .replaceAll(':insertZ:', zItem);
 
 
   
 
   if(customName.value !== '') {
     const name = customName.value;
-    newStory = newStory.replace("Bob", name)
+    newStory = newStory.replaceAll('Bob', name)
 
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300/14) + ' stone ';
-    const temperature =  Math.round(94 - 32 * (5/9)) + ' centigrade ';
-    temperature.replace('94 fahrenheit', temperature)
-    weight.replace('300 pounds', weight)
+    const weight = `${Math.round(300/14)} stone `;
+    const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
+    newStory = newStory.replaceAll('94 fahrenheit', temperature)
+    newStory = newStory.replaceAll('300 pounds', weight)
 
   }
 
